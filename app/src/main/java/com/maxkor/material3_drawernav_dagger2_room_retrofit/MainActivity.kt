24f313d072cf5ed005ddd8a5faf2117c1ea990bc
M4XK8R <kpopmyhope@gmail.com>
2023-11-17 07:14:47 +0300

    Initial commit

---
+++ app/src/main/java/com/maxkor/material3_drawernav_dagger2_room_retrofit/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.maxkor.material3_drawernav_dagger2_room_retrofit
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.maxkor.material3_drawernav_dagger2_room_retrofit.ui.theme.Material3DrawerNavDagger2RoomRetrofitTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Material3DrawerNavDagger2RoomRetrofitTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
+
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Material3-DrawerNav-Dagger2-Room-Retrofit"
+include(":app")
+ 
